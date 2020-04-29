dc69e126fec72842728403640e50274f912ba8fb
David <527425281@qq.com>
2020-04-29 11:29:28 +0800

    fix some bugs

--- Milestone2/ddl_temp.sql
+++ Milestone2/ddl_temp.sql
@@ -1,39 +1,39 @@
 CREATE TABLE Game (
-    id INT(5),
-    name VARCHAR(20) NOT NULL,
+    id INT(10),
+    name VARCHAR(256) NOT NULL,
     release_year INT(4) NOT NULL,
-    developer VARCHAR(20) NOT NULL,
+    developer VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
 )
 
 CREATE TABLE Genre (
-    name VARCHAR(20),
+    name VARCHAR(256),
     PRIMARY KEY (name)
 )
 
 CREATE TABLE Game_genre (
-    game_id INT(5),
-    genre_name VARCHAR(20),
+    game_id INT(10),
+    genre_name VARCHAR(256),
     PRIMARY KEY (id, genre_name),
     CONSTRAINT FOREIGN KEY game_id REFERENCES Game(id),
     CONSTRAINT FOREIGN KEY genre_name REFERENCES Genre(name)
 )
 
 CREATE TABLE Rating (
-    id INT(6),
-    user_rating DECIMAL(3),
-    website_rating DECIMAL(3),
+    id INT(10),
+    user_rating DECIMAL(10),
+    website_rating DECIMAL(10),
     PRIMARY KEY (id)
 )
 
 CREATE TABLE Platform (
-    name VARCHAR(20),
+    name VARCHAR(256),
     PRIMARY KEY (name)
 )
 
 CREATE TABLE Game_plat (
-    game_id INT(6),
-    plat_name VARCHAR(20),
+    game_id INT(10),
+    plat_name VARCHAR(256),
     PRIMARY KEY (id, plat_name),
     CONSTRAINT FOREIGN KEY game_id REFERENCES Game(id),
     CONSTRAINT FOREIGN KEY plat_name REFERENCES Platform(name)
@@ -41,21 +41,21 @@ CREATE TABLE Game_plat (
 
 
 CREATE TABLE Classification (
-    name VARCHAR(20),
+    name VARCHAR(256),
     description VARCHAR(256),
     PRIMARY KEY (name)
 )
 
 CREATE TABLE Game_class (
-    game_id INT(6),
-    class_name VARCHAR(20),
+    game_id INT(10),
+    class_name VARCHAR(256),
     PRIMARY KEY (id, class_name),
     CONSTRAINT FOREIGN KEY game_id REFERENCES Game(id),
     CONSTRAINT FOREIGN KEY class_name REFERENCES Classification(name)
 )
 
 CREATE TABLE Website (
-    id INT(6),
+    id INT(10),
     vg_url VARCHAR(256),
     photo_url VARCHAR(256)
     PRIMARY KEY (id)
